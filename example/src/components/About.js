5cff7c527d1f63745a92cf67583a49b296147cb9
cerico <garethrobertlee@gmail.com>
2018-01-05 11:47:22 +0000

    about

--- example/src/components/About.js
+++ example/src/components/About.js
@@ -82,40 +82,30 @@ class About extends React.Component  {
 
         <div style={style.blurb}>
         Firstly, install the package<br/>
-        <pre style={style.innerCode}>➜  musicali git:(dev) ✗ npm install hello-cards</pre>
+        <pre style={style.innerCode}>➜  musicali git:(dev) ✗ npm install charles-lamb-layout</pre>
        </div>
 
         <div style={style.blurb}>Secondly import into your project<br/>
 
-        <pre style={style.innerCode}>import Card from 'hello-cards';</pre>
+        <pre style={style.innerCode}>import Layout from 'charles-lamb-layout';</pre>
         </div>
 
         <div style={style.blurb}>Thirdly, include the Card component in your render method<br/>
-        <pre style={style.innerCode}>{`<Card product={product}/>`}</pre></div>
+        <pre style={style.innerCode}>{`<Layout links={links}/>`}</pre></div>
         {/* <img style={style.img} src="https://s3.eu-west-2.amazonaws.com/io1937/screenshots/include.jpeg"/><br/></div> */}
 
-        <div style={style.blurb}>Fourth, pass it a product object, here is an example product object<br/>
+        <div style={style.blurb}>Fourth, pass it your links<br/>
         <pre style={style.outerCode}>
-          <code style={style.code}>{`const product = {`}</code><br/>
-          <code style={style.code}>{`  name: "Musico",`}</code><br/>
-          <code style={style.code}>{`  page:  '/rocogo',`}</code><br/>
-          <code style={style.code}>{`  description: "A music player",`}</code><br/>
-          <code style={style.code}>{`  color:  "rgb(246, 235, 213)",`}</code><br/>
-          <code style={style.code}>{`  cols:  "2",`}</code><br/>
-          <code style={style.code}>{`  direction:  'row',`}</code><br/>
-          <code style={style.code}>{`  background:  'rgb(79, 92, 191)',`}</code><br/>
-          <code style={style.code}>{`  url:  "http://musico.malham.io",`}</code><br/>
-          <code style={style.code}>{`  logo: 'http://malham.io/roco-wide.png',`}</code><br/>
-          <code style={style.code}>{`  textOne: "React",`}</code><br/>
-          <code style={style.code}>{`  textTwo: "Redux",`}</code><br/>
-          <code style={style.code}>{`  number: 12`}</code><br/>
-          <code style={style.code}>{`}`}</code>
+          <code style={style.code}>{`const links = [`}</code><br/>
+          <code style={style.code}>{`  { path: "/", title: "home", key: 1 },`}</code><br/>
+          <code style={style.code}>{`  { path: "/about", title: "about", key: 2 }`}</code><br/>
+          <code style={style.code}>{`];`}</code>
         </pre>
         
         </div>
 
-        <div style={style.blurb}>Full example code at <a href="https://github.com/cerico/hello-cards">https://github.com/cerico/hello-cards</a><br/>
-        <div style={style.blurb}>GitHub README is  <a href="https://github.com/cerico/hello-cards/blob/master/README.md">here</a></div>
+        <div style={style.blurb}>Full example code at <a href="https://github.com/cerico/charles-lamb-layout">https://github.com/cerico/charles-lamb-layout</a><br/>
+        <div style={style.blurb}>GitHub README is  <a href="https://github.com/cerico/charles-lamb-layout/blob/master/README.md">here</a></div>
         </div>
       </div> 
     )
